1ce45cb7e4aa32b5a09335a9815dd80569bfa407
Mohammed Aljasser <justmo59@gmail.com>
2019-01-27 19:53:15 +0300

    registering the nodes

---
+++ dev/networkNodes.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./Blockchain');
+const port = process.argv[2];
+const uuid = require('uuid/v1');
+const rp = require('request-promise');
+
+const nodeAddress = uuid().split('-').join('');
+
+const bitcoin = new Blockchain();
+// body parser stuff
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+app.get('/blockchain', function (req, res) {
+  res.send(bitcoin);
+});
+ 
+app.post('/transaction', function (req, res) {
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount,req.body.sender, req.body.recipient);
+  res.json(`the transaction will be added in block : ${blockIndex}. `);
+});
+ 
+app.get('/mine', function (req, res) {
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+      transactions : bitcoin.pendingTransactions,
+      index: lastBlock['index'] +1 
+
+    };
+
+    const nonce = bitcoin.proofOfWork(previousBlockHash,currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce);
+    bitcoin.createNewTransaction(12.5,'00',nodeAddress);
+    const newBlock = bitcoin.createNewBlock(nonce , previousBlockHash,blockHash);
+    res.json({
+      note: 'Block mined',
+      block: newBlock
+
+    });
+
+    app.post('/register-and-broadcast-node', function (req, res) {
+      const newNodeUrl = req.body.newNodeUrl;
+     if(bitcoin.newtworkNodes.indexOf(newNodeUrl) == -1) bitcoin.newtworkNodes.push(newNodeUrl);
+      const regNodesPromises= [];
+     bitcoin.newtworkNodes.forEach(networkNodeUrl =>{
+        const requestOptions ={
+          uri: newNodeUrl + '/register-node',
+          method:'POST',
+          body:{newNodeUrl:newNodeUrl},
+          json:true
+
+      };
+         regNodesPromises.push(rp(requestOptions));
+      
+     });
+
+     Promise.all(regNodesPromises)
+     .then(data=>{
+
+      
+     })
+    });
+
+    app.post('/register-node', function (req, res) {
+        
+    });
+
+    app.post('/register-node-bulk', function (req, res) {
+        
+    });
+
+
+});
+app.listen(port ,function (){
+
+console.log(`server listening on ${port}...`);
+});
