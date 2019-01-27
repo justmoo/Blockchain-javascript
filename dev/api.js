4c0e3a89ead032483923d64438c4ffac65cf6242
Mohammed Aljasser <justmo59@gmail.com>
2019-01-27 18:18:52 +0300

    added blockchain route

--- dev/api.js
+++ dev/api.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./Blockchain');
 
+const bitcoin = new Blockchain();
 // body parser stuff
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
  
-app.get('/', function (req, res) {
-  res.send('welcome');
+app.get('/blockchain', function (req, res) {
+  res.send(bitcoin);
 });
  
 app.post('/send', function (req, res) {
